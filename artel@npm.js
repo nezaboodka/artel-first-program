b5c4fb0707a38d0f5c822ebe00a3b0bcf78def2c
Yury Chetyrko <ychetyrko@gmail.com>
2023-09-24 00:10:52 +0300

    Косметика

--- artel@npm.js
+++ artel@npm.js
@@ -2,20 +2,15 @@ const fs = require("fs")
 const cp = require("child_process")
 
 const nodeModulesPath = "node_modules"
-
 let isUpToDate = fs.existsSync(nodeModulesPath)
 if (isUpToDate) {
   const packageJsonPath = "package.json"
   const packageLockJsonPath = "package-lock.json"
-
   const m = fs.statSync(nodeModulesPath)
   const p1 = fs.statSync(packageJsonPath)
   const p2 = fs.statSync(packageLockJsonPath)
-  isUpToDate =
-    m.mtime >= p1.mtime &&
-    m.mtime >= p2.mtime &&
-    m.mtime >= p1.ctime &&
-    m.mtime >= p2.ctime
+  isUpToDate = m.mtime >= p1.mtime && m.mtime >= p2.mtime &&
+    m.mtime >= p1.ctime && m.mtime >= p2.ctime
 }
 
 if (!isUpToDate) {
@@ -31,4 +26,8 @@ else
   console.log("\n")
 
 const cli = require("artel/build/artel-cli.js")
-// art.awaiter.then(() => console.log("\n\n"), () => console.log("\n\n"))
+cli.awaiter.then(result => {
+  // do nothing
+}, error => {
+  // do nothing
+})
